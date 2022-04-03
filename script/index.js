d230343474b22a721d224a5fde081b41394120a6
Maksim Nikolaev <mksniklaev@gmail.com>
2022-04-03 12:53:36 +0500

    fix: index.js

--- script/index.js
+++ script/index.js
@@ -3,14 +3,14 @@ const btnMagistarture = document.querySelector('.education__button_type_magistra
 const containerAspiranture = document.querySelector('.education__container_type_aspiranture');
 const containerMagistrature = document.querySelector('.education__container_type_magistrature');
 
-btnAspiranture.addEventListener('click', () =>{
+btnAspiranture.addEventListener('click', () => {
   btnAspiranture.classList.add('education__button_active');
   containerMagistrature.classList.add('education__container_visible');
   btnMagistarture.classList.remove('education__button_active');
   containerAspiranture.classList.remove('education__container_visible');
 })
 
-btnMagistarture.addEventListener('click', () =>{
+btnMagistarture.addEventListener('click', () => {
   btnMagistarture.classList.add('education__button_active');
   containerAspiranture.classList.add('education__container_visible');
   btnAspiranture.classList.remove('education__button_active');
