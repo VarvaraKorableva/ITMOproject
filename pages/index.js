e1d4e8935a2b21eb7fda489b1ad2c9282e4c1714
aleksandree128 <aleksandree128@mail.ru>
2022-04-03 16:36:00 +0500

    feat: add indactrial

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+const popups=document.querySelector(".card")
+const newAbout=document.querySelector(".popup_type_about")
+const openPopupButton=document.querySelector(".card__btn")
+const closePopupModal=newAbout.querySelector(".popup__close")
+
+
+
+
+function openPopup(modal) {
+  modal.classList.add("popup_opened");
+  document.addEventListener("keydown", keyClose);
+}
+
+function closePopup(modal) {
+  modal.classList.remove("popup_opened");
+  document.removeEventListener("keydown", keyClose);
+}
+
+function keyClose(evt) {
+  if (evt.key === "Escape") {
+    const openedPopup = document.querySelector(".popup_opened");
+    closePopup(openedPopup);
+  }
+}
+openPopupButton.addEventListener("click",()=>
+openPopup(newAbout))
+closePopupModal.addEventListener("click",()=>{
+  closePopup(newAbout)
+})
